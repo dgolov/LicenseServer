4fcf5b3da81541b6a7fa231a71cdf0276161f6e6
dgolov <dmitriy.evgenyevich@gmail.com>
2022-11-08 16:09:26 +0600

    Added config

--- cmd/main.go
+++ cmd/main.go
@@ -3,17 +3,27 @@ package main
 import (
 	"github.com/dgolov/LicenseServer"
 	"github.com/dgolov/LicenseServer/pkg/handler"
+	"github.com/spf13/viper"
 	"log"
 )
 
 // Main
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing config file: %s", err.Error())
+	}
+
 	handlers := new(handler.Handler)
 	srv := new(LicenseServer.Server)
 
-	if err := srv.Run("8001", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error while runing http server: %s", err.Error())
-		return
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
