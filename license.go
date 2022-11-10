7dbadfed6600f3094e1e417108a405cb3a5d3a68
dgolov <dmitriy.evgenyevich@gmail.com>
2022-11-10 16:39:00 +0600

    Added reading db for struct

--- license.go
+++ license.go
@@ -4,10 +4,10 @@ import "time"
 
 type License struct {
 	// License model
-	Id                 int       `json:"-"`
-	Uuid               string    `json:"uuid"`
-	HardwareParameters string    `json:"hardware_parameters"`
-	IsActive           bool      `json:"is_active"`
-	ActivatedOn        time.Time `json:"activated_on"`
-	ExpiratedOn        time.Time `json:"expirated_on"`
+	Id                 int       `json:"-" db:"id"`
+	Uuid               string    `json:"license_uuid" db:"uuid" binding:"required"`
+	HardwareParameters string    `json:"hardware_parameters" db:"hardware_parameters" binding:"required"`
+	IsActive           bool      `json:"is_active" db:"is_active"`
+	ActivatedOn        time.Time `json:"activated_on" db:"activated_on"`
+	ExpiratedOn        time.Time `json:"expirated_on" db:"expirated_on"`
 }
